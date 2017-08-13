41b2aa0af737c281c74de02ec94b8b9ddcedf9cd
Kai Gittens <kai.gittens@gmail.com>
2017-08-13 09:11:31 -0400

    update webpack build process (see message) -install babel-based config packages -config babel/webpack build with .babelrc -build out files in js-build/ to js/

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js-build/**.js',
+  module : {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ],
+  },
+  entry: './js-build/helpers.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '_site/js')
+    path: path.resolve(__dirname, 'js')
   }
 };
