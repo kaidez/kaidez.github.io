2bfd6c27041de608d894e350affa81d4de542bbe
Kai Gittens <kai.gittens@gmail.com>
2017-08-19 10:18:42 -0400

    update syntax for ES6 reasons

--- js-build/helpers.js
+++ js-build/helpers.js
@@ -2,7 +2,7 @@ const getPostDiv =  document.querySelectorAll(".post-link-hook");
 
 const doEventOnElement = (element, getEvent, fn) => {
   for (let i = 0; i < element.length; i++) {
-    element[i].addEventListener(getEvent, (event) => {
+    element[i].addEventListener(getEvent, event => {
       fn(element[i])
     })
   }
@@ -10,7 +10,7 @@ const doEventOnElement = (element, getEvent, fn) => {
 
 function goToPage(el) {
   const getArticleLink = el.dataset.url
-  window.location = getArticleLink;
+  window.location = getArticleLink
 }
 
 export const divClick = doEventOnElement(getPostDiv, 'click', goToPage)
