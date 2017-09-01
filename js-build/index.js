39c2513ceec9fe790f2cc02843937e5303dd83c7
Kai Gittens <kai.gittens@gmail.com>
2017-09-01 06:23:45 -0400

    remove uneeded method call

--- js-build/index.js
+++ js-build/index.js
@@ -4,9 +4,6 @@ import { render } from 'react-dom'
 import { divClick } from "./helpers"
 import { Navigation } from "./components/Navigation"
 
-// Run divClick code
-divClick
-
 // Load <Navigation /> into <nav id="navReactEntryPoint" />
 render(
   <Navigation />,
