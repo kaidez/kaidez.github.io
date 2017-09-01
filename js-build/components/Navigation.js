7f465611287128f68f37e30e2a84c02b836a616e
Kai Gittens <kai.gittens@gmail.com>
2017-09-01 06:24:33 -0400

    start creating repsonsive menu (see message) create a new Sass partial called "_sass/kaidez/_max-width-767.scss" and list it in "_sass/kaidez.scss." put responsive CSS styling in that new partial and add/update styles in "_sass/kaidez/_layout.scss."  update layout in the React component in "js-build/components/Navigation.js."

--- js-build/components/Navigation.js
+++ js-build/components/Navigation.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export const Navigation = () => (
   <div>
     <h2 className="header--hidden-text">Main Navigation</h2>
-    <div className="trigger">
+    <div className="header__menu" tabIndex="0">
+      <span className="header--menu-line"></span>
+      <span className="header--menu-line"></span>
+    </div>
+    <div className="header__nav-wrapper">
       <a className="header__nav--page-link" href="/blog">blog</a>
       <a className="header__nav--page-link" href="/articles">articles</a>
       <a className="header__nav--page-link" href="/lynda-kaidez">kaidez on lynda.com</a>
