1d79873c371ebe97972167232df29e2c2203a379
Kai Gittens <kai.gittens@gmail.com>
2017-09-02 07:11:47 -0400

    start building React mobile menu (see message) Refactor the Navigation class from a stateless component to a class-based one.  Add constructor method to include two menu toggle states (but one may be removed when the code is completed). Create internal "toggleMobileMenu()" method to toggle menu states that show & hide the menu. Test that it works before creating the final menu.

--- js-build/components/Navigation.js
+++ js-build/components/Navigation.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
 
-export const Navigation = () => (
-  <div>
-    <h2 className="header--hidden-text">Main Navigation</h2>
-    <div className="header__menu" tabIndex="0">
-      <span className="header--menu-line"></span>
-      <span className="header--menu-line"></span>
-    </div>
-    <div className="header__nav-wrapper">
-      <a className="header__nav--page-link" href="/blog">blog</a>
-      <a className="header__nav--page-link" href="/articles">articles</a>
-      <a className="header__nav--page-link" href="/lynda-kaidez">kaidez on lynda.com</a>
-      <a className="header__nav--page-link" href="/ajax-tutorial">ajax tutorial</a>
-    </div>
-  </div>
-)
+export class Navigation extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isToggleOn: true,
+      isMenuVisible: false
+    }
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this)
+  }
+
+  toggleMobileMenu(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    e || e.window.event
+    const clickTarget = document.querySelector("#menuButton")
+
+    clickTarget
+    ?
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn,
+      isMenuVisible: !prevState.isMenuVisible
+    }))
+    :
+    null
+  }
+
+  render() {
+    return(
+      <div>
+        <h2 className="header--hidden-text">Main Navigation</h2>
+        <div id="menuButton" className="header__menu" onClick={this.toggleMobileMenu}>
+          <span className="header--menu-line"></span>
+          <span className="header--menu-line"></span>
+        </div>
+        {
+          this.state.isMenuVisible
+          ?
+          <span>menu is visible</span>
+          :
+          null
+        }
+        <div className="header__nav-wrapper">
+          <a className="header__nav--page-link" href="/blog">blog</a>
+          <a className="header__nav--page-link" href="/articles">articles</a>
+          <a className="header__nav--page-link" href="/lynda-kaidez">kaidez on lynda.com</a>
+          <a className="header__nav--page-link" href="/ajax-tutorial">ajax tutorial</a>
+        </div>
+      </div>
+    )
+  }
+}
