c54a1d50318e2dfff11a2b8e62f88fdaa9388212
Kai Gittens <kai.gittens@gmail.com>
2017-09-02 21:09:34 -0400

    update build & watch tasks

--- js-build/components/NavigationChildComponents/Menu.js
+++ js-build/components/NavigationChildComponents/Menu.js
@@ -18,7 +18,7 @@ class Menu extends Component {
       >
         <a className="header__nav--page-link" href="/blog">blog</a>
         <a className="header__nav--page-link" href="/articles">articles</a>
-        <a className="header__nav--page-link" href="/lynda-kaidez">kaidez on lynda.com foo</a>
+        <a className="header__nav--page-link" href="/lynda-kaidez">kaidez on lynda.com</a>
         <a className="header__nav--page-link" href="/ajax-tutorial">ajax tutorial</a>
       </div>
     )
