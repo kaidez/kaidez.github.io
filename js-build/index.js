229a54080d5f9c9780817c25d2d9268e9d2315f8
Kai Gittens <kai.gittens@gmail.com>
2017-09-02 20:43:46 -0400

    rearrange imports

--- js-build/index.js
+++ js-build/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Navigation } from "./components/Navigation"
 
 import { divClick } from "./helpers"
-import { Navigation } from "./components/Navigation"
 
 // Load <Navigation /> into <nav id="navReactEntryPoint" />
 render(
