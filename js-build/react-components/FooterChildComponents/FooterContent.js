0b78063726bbaeb35239b88f20274c3479b5790b
Kai Gittens <kai.gittens@gmail.com>
2017-11-26 21:34:48 -0500

    update core js file

--- js-build/react-components/FooterChildComponents/FooterContent.js
+++ js-build/react-components/FooterChildComponents/FooterContent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome';
 
 class FooterContent extends Component {
 
@@ -43,6 +44,33 @@ class FooterContent extends Component {
         <div className="footer__column footer__column--3">
           Kai "kaidez" Gittens is a front-end web developer for JPMorgan Chase. He has contributed to web experiences for Revlon, jetBlue, Everyday Health and the United Nations. He is a former JavaScript instructor for lynda.com and is a loyal supporter of Chelsea F.C.
         </div>
+        <ul>
+          <li>
+            <FontAwesome
+              name='twitter'
+              size='2x'
+            />
+          </li>
+          <li>
+            <FontAwesome
+              name='facebook'
+              size='2x'
+            />
+          </li>
+          <li>
+            <FontAwesome
+              name='github'
+              size='2x'
+            />
+          </li>
+          <li>
+            <FontAwesome
+              name='youtube'
+              size='2x'
+            />
+          </li>
+      </ul>
+
 
         <div className="footer__colophon">
           <span className="footer__colophon--item">&copy;2008-17</span>
