45b8197434b47f8664bb3751c68007f4910b21e6
Kai Gittens <kai.gittens@gmail.com>
2018-02-15 19:59:58 -0500

    update click to tweet sample code

--- samples/click-to-tweet/04/tweetButton.js
+++ samples/click-to-tweet/04/tweetButton.js
@@ -1,11 +1,19 @@
+// tweetButton.js
+
 var getPostTitle = document.getElementById("blog-post-title").innerHTML,
-    linkElement = document.getElementById("tweet-this-post");
+  linkElement = document.getElementById("tweet-this-post");
 
 $(linkElement).click(function(event){
 
   event.preventDefault();
 
-  var tweetedLink = window.location.href;
+  var tweetedLink;
+
+  if(!window.location.origin) {
+    tweetedLink = window.location.protocol + "//" + window.location.hostname + window.location.pathname;
+  } else {
+    tweetedLink = window.location.origin + window.location.pathname;
+  }
 
   window.open("http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
 
