161a4c5dbeeb9432708141c4e361402dffb1c34d
Kai Gittens <kai.gittens@gmail.com>
2022-02-27 12:43:48 -0500

    JavaScript refactor * bump/add dependencies * update .babelrc to compile react better * refactor <Footer />

--- js-build/index.js
+++ js-build/index.js
@@ -1,16 +1,13 @@
 //# sourceURL=index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Navigation } from './react-components/Navigation'
-import { Footer } from './react-components/Footer'
-
-import { divClick } from './helpers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Navigation } from './react-components/Navigation';
+import { Footer } from './react-components/Footer';
+// Not being used right now
+// import { divClick } from './helpers';
 
 // Load <Navigation /> into <nav id='navReactEntryPoint' />
-ReactDOM.render(Navigation, document.getElementById('navReactEntryPoint'));
+ReactDOM.render(<Navigation />, document.getElementById('navReactEntryPoint'));
 
 // Load <Footer /> into <nav id='footerReactEntryPoint' />
-ReactDOM.render(
-  Footer,
-  document.getElementById('footerReactEntryPoint')
-)
+ReactDOM.render(<Footer />, document.getElementById('footerReactEntryPoint'));
--- js-build/react-components/Footer.js
+++ js-build/react-components/Footer.js
@@ -1,57 +1,50 @@
 //# sourceURL=Footer.js
-import React, { Component } from 'react'
-import FooterContent from './FooterChildComponents/FooterContent'
+import React, { Component } from 'react';
+import FooterContent from './FooterChildComponents/FooterContent';
 
 export class Footer extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      isFooterHiddenOnMobile: true
-    }
+      isFooterHiddenOnMobile: true,
+    };
   }
-
-  static displayName = 'Footer'
 
   toggleFooter = (e) => {
-    e || e.window.event
-    const clickTarget = document.querySelector("#footerButton")
+    e || e.window.event;
+    const clickTarget = document.querySelector('#footerButton');
 
     clickTarget
-    ?
-    this.setState(prevState => ({
-      isFooterHiddenOnMobile: !prevState.isFooterHiddenOnMobile
-    }))
-    :
-    null
-  }
+      ? this.setState((prevState) => ({
+          isFooterHiddenOnMobile: !prevState.isFooterHiddenOnMobile,
+        }))
+      : null;
+  };
 
   render() {
-    return(
+    return (
       <div
         className={
           !this.state.isFooterHiddenOnMobile
-          ?
-          "footer__wrapper"
-          :
-          "footer__wrapper footer--has-cursor"
+            ? 'footer__wrapper'
+            : 'footer__wrapper footer--has-cursor'
         }
       >
         <p
-          id="footerButton"
+          id='footerButton'
           onClick={this.toggleFooter}
           className={
             !this.state.isFooterHiddenOnMobile
-            ?
-            "footer__button--isHidden"
-            :
-            "footer__button"
+              ? 'footer__button--isHidden'
+              : 'footer__button'
           }
         >
           Click here to see the footer
         </p>
-        <FooterContent isFooterHiddenOnMobile={this.state.isFooterHiddenOnMobile} />
+        <FooterContent
+          isFooterHiddenOnMobile={this.state.isFooterHiddenOnMobile}
+        />
       </div>
-    )
+    );
   }
 }
