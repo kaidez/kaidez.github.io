e7e41c899fbd85b2796a7b65628452e02d212b73
Kai Gittens <kai.gittens@gmail.com>
2022-02-28 08:01:05 -0500

    update JS build

--- js-build/index.js
+++ js-build/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './react-components/Navigation';
 import Footer from './react-components/Footer';
-// Not being used right now
-// import { divClick } from './helpers';
+import { divClick } from './helpers';
 
 // Load <Navigation /> into <nav id='navReactEntryPoint' />
 ReactDOM.render(<Navigation />, document.getElementById('navReactEntryPoint'));
