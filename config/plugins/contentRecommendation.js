5f5021ec0ddc7a2314800d7b02bc65d971f47127
Kai Gittens <kaidezblog@gmail.com>
2025-08-23 17:11:50 -0400

    clean up code

--- config/plugins/contentRecommendation.js
+++ config/plugins/contentRecommendation.js
@@ -20,9 +20,7 @@ module.exports = function (eleventyConfig) {
           });
         }
 
-        
-      // Remove duplicates and limit to a specific number of related posts, for instance, 3
-
+        //Remove duplicates and limit to a specific number of related posts, for instance, 3
         related = [...new Set(related)].slice(0, 3);
 
         post.data.related = related;
